b6e0158590bf5c93e5c2ad8de9c43d67bc435942
shinyo-dc <duongchan159@gmail.com>
2020-08-17 18:21:06 +0700

    First WebGL game created based on the course, added the function to quit the game by press Q

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour 
+{
+    // SerializeField make the variable available within the inspector to be edited and viewed
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+    bool isQuit = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ManageStates();	
+	}
+
+    private void ManageStates()
+    {
+        var nextStates = state.GetNextStates();
+        if ( !isQuit )
+        {
+            // Using loop to process to the states available in the array
+            for ( int index = 0; index < nextStates.Length; ++index )
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+                {
+                    state = nextStates[index];
+                }
+            }
+            if ( Input.GetKeyDown(KeyCode.Q) )
+            {
+                isQuit = true;
+            }
+            textComponent.text = state.GetStateStory();
+        }
+        else if ( isQuit )
+        {
+            var currentState = state;
+            textComponent.text = "Are you sure you want to quit? \n \n" +
+                                 "1. Yes \n" +
+                                 "2. No";
+            if ( Input.GetKeyDown(KeyCode.Alpha1) )
+            {
+                isQuit = false;
+                state = startingState;
+            }
+            else if ( Input.GetKeyDown(KeyCode.Alpha2) )
+            {
+                isQuit = false;
+                state = currentState; 
+            }
+        }
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adding State as a scriptable object to be created as an asset
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    // TextArea: changing the size of the textbox in the inspector, (minLines, maxLines)
+    [TextArea(10, 12)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    // Public method
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;        
+    }
+}
